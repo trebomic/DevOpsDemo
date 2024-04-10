e7988e1b7f96ba9ad0fbdecf1a7c80a42adc45dd
trebomic <58138261+trebomic@users.noreply.github.com>
2024-04-10 23:50:51 +0200

    2. Backend changes

--- backend/src/main/java/ch/zhaw/iwi/devops/demo/ToDoController.java
+++ backend/src/main/java/ch/zhaw/iwi/devops/demo/ToDoController.java
@@ -5,7 +5,6 @@ import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.web.bind.annotation.*;
@@ -14,15 +13,15 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class ToDoController {
 
-    private final Map<Integer, ToDo> todos = new HashMap<>();
+    private Map<Integer, ToDo> todos = new HashMap<>();
 
     @EventListener(ApplicationReadyEvent.class)
     public void init() {
-        this.todos.put(1, new ToDo(1, "Neuer Job", "5 DevOps Engineers einstellen"));
-        this.todos.put(2, new ToDo(2, "Geschäftsleitung", "Mit Präsentation von DevOps überzeugen"));
-        this.todos.put(3, new ToDo(3, "Unit Tests", "Neues Projekt mit Unit Tests starten"));
-        this.todos.put(4, new ToDo(4, "Deployment", "Jede Woche!"));
-        this.todos.put(5, new ToDo(5, "Organigramm", "Löschen"));
+        todos.put(1, new ToDo(1, "Neuer Job", "5 DevOps Engineers einstellen"));
+        todos.put(2, new ToDo(2, "Geschäftsleitung", "Mit Präsentation von DevOps überzeugen"));
+        todos.put(3, new ToDo(3, "Unit Tests", "Neues Projekt mit Unit Tests starten"));
+        todos.put(4, new ToDo(4, "Deployment", "Jede Woche!"));
+        todos.put(5, new ToDo(5, "Organigramm", "Löschen"));
         System.out.println("Init Data");
     }
 
@@ -33,19 +32,20 @@ public class ToDoController {
 
     @GetMapping("/services/ping")
     public String ping() {
-        return String.format("{ \"status\": \"ok\", \"userId\": \"admin\", \"languageCode\": \"%s\",\"version\": \"0.0.1\"}", "de");
+        String languageCode = "de";
+        return String.format("{ \"status\": \"ok\", \"userId\": \"admin\", \"languageCode\": \"%s\",\"version\": \"0.0.1\"}", languageCode);
     }
 
     @GetMapping("/count")
     public int count() {
-        return this.todos.size();
+        return todos.size();
     }
 
     @GetMapping("/services/todo")
     public List<PathListEntry<Integer>> todo() {
         List<PathListEntry<Integer>> result = new ArrayList<>();
-        this.todos.values().forEach(todo -> {
-            PathListEntry<Integer> entry = new PathListEntry<>();
+        todos.values().forEach(todo -> {
+            var entry = new PathListEntry<Integer>();
             entry.setKey(todo.getId(), "todoKey");
             entry.setName(todo.getTitle());
             entry.getDetails().add(todo.getDescription());
@@ -57,36 +57,31 @@ public class ToDoController {
 
     @GetMapping("/services/todo/{key}")
     public ToDo getTodo(@PathVariable Integer key) {
-        return this.todos.get(key);
+        return todos.get(key);
     }
 
     @PostMapping("/services/todo")
     public void createTodo(@RequestBody ToDo todo) {
-        int newId = this.todos.keySet().stream().max(Comparator.naturalOrder()).orElse(0) + 1;
+        int newId = todos.keySet().stream().max(Comparator.naturalOrder()).orElse(0) + 1;
         todo.setId(newId);
-        this.todos.put(newId, todo);
+        todos.put(newId, todo);
     }
 
     @PutMapping("/services/todo/{id}")
     public void createTodo(@PathVariable Integer id, @RequestBody ToDo todo) {
         todo.setId(id);
-        this.todos.put(id, todo);
+        todos.put(id, todo);
     }
 
     @DeleteMapping("/services/todo/{key}")
     public ToDo deleteTodo(@PathVariable Integer key) {
-        return this.todos.remove(key);
+        return todos.remove(key);
     }
 
     @GetMapping("/services/todo/search")
     public List<ToDo> searchTodosByTitle(@RequestParam String title) {
-        List<ToDo> filteredTodos = new ArrayList<>();
-        for (ToDo todo : this.todos.values()) {
-            if (todo.getTitle().toLowerCase().contains(title.toLowerCase())) {
-                filteredTodos.add(todo);
-            }
-        }
-        return filteredTodos;
+        return todos.values().stream()
+                .filter(todo -> todo.getTitle().toLowerCase().contains(title.toLowerCase()))
+                .toList();
     }
-
 }
