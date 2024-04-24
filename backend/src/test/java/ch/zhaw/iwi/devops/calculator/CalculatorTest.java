aaaee30b00f60eea10158ea167f1d67844fb92e6
trebomic <58138261+trebomic@users.noreply.github.com>
2024-04-24 15:02:56 +0200

    Add tests

--- backend/src/test/java/ch/zhaw/iwi/devops/calculator/CalculatorTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/calculator/CalculatorTest.java
@@ -1,6 +1,22 @@
 package ch.zhaw.iwi.devops.calculator;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class CalculatorTest {
 
+    @Test
+    public void testAdd() {
+        Calculator calculator = new Calculator();
+        int result = calculator.add(10, 20);
+        Assertions.assertEquals(30, result);
+    }
+
+    @Test
+    public void testSubtract() {
+        Calculator calculator = new Calculator();
+        int result = calculator.subtract(20, 10);
+        Assertions.assertEquals(10, result);
+    }
 }
 
